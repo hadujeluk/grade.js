4db84eee7bc3da1a052848cff179769b593ba21b
hadujeluk <162443382+hadujeluk@users.noreply.github.com>
2024-03-27 11:10:32 +0300

    Update grade.js

--- grade.js
+++ grade.js
@@ -1,17 +1,22 @@
-function gradeMarks(marks, grade){
-    let marks = prompt ("input student's marks (0-100)")
-    if (marks >= 79){
-       const grade = 'A';
-    }else if (marks >= 60){
-       const grade = 'B';
-    }else if (marks >= 49){
-       const grade = 'C';
-    }else if (marks >=40){
-        const grade = 'D';
-    }else if (marks < 40){
-        grade = 'E'
-    }
-    console.log(marks , grade)
+// student grader
+function studentGrader(score){
+    if( score>100 || score<0 ){
+        console.log("invalid score")
+        return "invalid score"
+    }else if ( score > 79) {
+        grade ='A'
+    }else if (score >=60 && score <=79)  {
+        grade='B'
+    }else if (score >=49 && score <= 59) {
+        grade= 'C'
+    }else if (score >=39 && score < 49) {
+        grade= 'D'
+    }else if (score <=38) {
+        grade= 'E'
+    }return grade
+}   
+
+console.log(studentGrader())
 
 
 
@@ -26,4 +31,3 @@ function gradeMarks(marks, grade){
 
 
 
-}
